91437b88f6788f84fb588355ba5220f358c83e11
tiffanylau117 <tiffanylau117@gmail.com>
2023-01-09 20:30:46 -0800

    added jquery & reponsive hamburger menu, changed homepage look

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 //  alert("Welcome to my portfolio");
- console.log("Hi folks")
+//  console.log("Hi folks")
+
+function menuToggle() {
+  var x = document.getElementById('navtoggle');
+  if (x.className === 'myNavtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'myNavtoggle';
+  }
+}
